143403b4058632ebf18a811a0c6f8c77171dd412
herrstrietzel <der@herrstrietzel.de>
2024-06-13 21:05:51 +0200

    update version

--- demo/el-bbox.js
+++ demo/el-bbox.js
@@ -1,32 +1,11 @@
 var pathDataBB = require("svg-pathdata-getbbox");
-var { getBBoxFromEl, getBBoxFromD, getPathDataBBox, getPathDataFromEl, parsePathDataNormalized } = pathDataBB;
+var { getBBoxFromEl, getBBoxFromD, getPathDataBBox, } = pathDataBB;
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
 let svgMarkup = `<svg id="svg" viewBox="0 0 100 100">
-<path fill="none" stroke="black" d="M3,7 
-  L13,7 
-  m-20,10 
-  l10,0 
-  V27 
-  H23 
-  v10 
-  h10
-  C 33,43 38,47 43,47 
-  c 0,5 5,10 10,10
-  S 63,67 63,67       
-  s -10,10 10,10
-  Q 50,50 73,57
-  q 20,-5 0,-10
-  T 70,40
-  t 0,-15
-  A 5, 10 45 10 40,20  
-  a5,5 20 01 -10,-10
-  Z  
-
-"/>
+<path fill="none" stroke="black" d="M3 7 13 7m-10 10 10 0V27H23v10h10C33 43 38 47 43 47c0 5 5 10 10 10S63 67 63 67s-10 10 10 10Q50 50 73 57q20-5 0-10T70 40t0-15A5 10 45 1040 20a5 5 20 01-10-10Z"/>
 </svg>`;
-
 
 const dom = new JSDOM(svgMarkup);
 let svg = dom.window.document.querySelector('svg')
--- get-bbox.js
+++ get-bbox.js
@@ -738,13 +738,12 @@
                 attNames = ['x', 'y', 'width', 'height', 'rx', 'ry'];
                 ({ x, y, width, height, rx, ry } = getAtts(attNames));
 
-
                 if (!rx && !ry) {
                     pathData = [
                         { type: "M", values: [x, y] },
-                        { type: "H", values: [x + width] },
-                        { type: "V", values: [y + height] },
-                        { type: "H", values: [x] },
+                        { type: "L", values: [x + width, y] },
+                        { type: "L", values: [x + width, y + height] },
+                        { type: "H", values: [x, y + height] },
                         { type: "Z", values: [] }
                     ];
                 } else {
@@ -758,13 +757,13 @@
 
                     pathData = [
                         { type: "M", values: [x + rx, y] },
-                        { type: "H", values: [x + width - rx] },
+                        { type: "L", values: [x + width - rx, y] },
                         { type: "A", values: [rx, ry, 0, 0, 1, x + width, y + ry] },
-                        { type: "V", values: [y + height - ry] },
+                        { type: "L", values: [x + width - rx, y + height - ry] },
                         { type: "A", values: [rx, ry, 0, 0, 1, x + width - rx, y + height] },
-                        { type: "H", values: [x + rx] },
+                        { type: "L", values: [x + rx, y + height] },
                         { type: "A", values: [rx, ry, 0, 0, 1, x, y + height - ry] },
-                        { type: "V", values: [y + ry] },
+                        { type: "L", values: [x, y + ry] },
                         { type: "A", values: [rx, ry, 0, 0, 1, x + rx, y] },
                         { type: "Z", values: [] }
                     ];
